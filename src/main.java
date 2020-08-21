5755bfcb208549d698a506053652de9bf35d2655
Adrian Sion <adriandavidsion@gmail.com>
2020-08-21 18:08:44 -0400

    Added integer for loop

--- src/main.java
+++ src/main.java
@@ -1,5 +1,13 @@
 public class main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello, world!");
+
+        int[] integers = new int[20];
+        for (int i = 0; i < integers.length; i++) {
+            integers[i] = i + 1;
+            System.out.print(integers[i] + " ");
+        }
+
+        System.out.println("");
     }
 }
